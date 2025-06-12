c86f8da3d7e1d29e78dadb984be1519caad0df5b
Pillow <m.farid.ft20@mail.umy.ac.id>
2025-06-12 17:10:53 +0700

    adding emptiness check on remove method

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -47,6 +47,13 @@ public:
 
     void remove()
     {
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+
     }
 };
 int main()
